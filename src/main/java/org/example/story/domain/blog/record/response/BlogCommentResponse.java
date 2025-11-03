e6663041a5e574c0ed46099bb4cbe2bdb9c9d7b3
hongjm0912~ <s25036@gsm.hs.kr>
2025-11-03 13:51:44 +0900

    feat(blog): 필드명 수정

--- src/main/java/org/example/story/domain/blog/record/response/BlogCommentResponse.java
+++ src/main/java/org/example/story/domain/blog/record/response/BlogCommentResponse.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 public record BlogCommentResponse(
         // 이름이 곧 역할인 record
         Long id,
-        Long portfolioId,
+        Long blogId,
         Long userId,
         String content,
         Instant createdAt
