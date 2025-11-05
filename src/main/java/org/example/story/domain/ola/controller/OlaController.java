047e957babf784dae8046a32662c38ca8515f4ca
hongjm0912~ <s25036@gsm.hs.kr>
2025-11-05 15:09:36 +0900

    feat(ola): 올라 피드백 기능 구현

---
+++ src/main/java/org/example/story/domain/ola/controller/OlaController.java
@@ -0,0 +1,26 @@
+package org.example.story.domain.ola.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.story.domain.ola.record.request.OlaRequest;
+import org.example.story.domain.ola.record.response.OlaListResponse;
+import org.example.story.domain.ola.record.response.OlaResponse;
+import org.example.story.domain.ola.service.OlaService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/ola")
+public class OlaController {
+    private final OlaService olaService;
+
+    @PostMapping("/{portfolio_id}")
+    public OlaResponse feed(@PathVariable("portfolio_id") Long portfolioId,
+                            @RequestBody OlaRequest request) {
+        return olaService.feedOla(request.question(), portfolioId);
+    }
+
+    @GetMapping("/history/{portfolio_id}")
+    public OlaListResponse history(@PathVariable("portfolio_id") Long portfolioId){
+        return olaService.historyOla(portfolioId);
+    }
+}
---
+++ src/main/java/org/example/story/domain/ola/service/OlaService.java
@@ -0,0 +1,79 @@
+package org.example.story.domain.ola.service;
+
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
+import org.example.story.domain.ola.entity.OlaHistoryJpaEntity;
+import org.example.story.domain.ola.record.response.OlaListResponse;
+import org.example.story.domain.ola.record.response.OlaResponse;
+import org.example.story.domain.ola.repository.OlaRepository;
+import org.example.story.domain.portfolio.entity.PortfolioJpaEntity;
+import org.example.story.domain.portfolio.repository.PortfolioRepository;
+import org.example.story.global.error.exception.ExpectedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OlaService {
+    private final OpenAiService openAiService;
+    private final PortfolioRepository portfolioRepository;
+    private final OlaRepository olaRepository;
+
+    public OlaResponse feedOla(String question, Long portfolioId) {
+        PortfolioJpaEntity portfolio = portfolioRepository.findById(portfolioId)
+                .orElseThrow(() -> new ExpectedException(HttpStatus.NOT_FOUND,"존재하지 않는 포트폴리오입니다"));
+
+        String systemMessage = "너는 사용자의 포트폴리오를 확인하고 피드백을 주는 ai 이름은 올라야. " +
+                "아래 사용자가 작성한 포트폴리오의 일부를 보고 이상적인 포트폴리오 되기위한 피드백을 제공해줘. "
+                + "피드백의 형식은 조언과 개선안의 예시를 보여주는 형식으로 부탁해";
+
+        ChatCompletionRequest request = ChatCompletionRequest.builder()
+                .model("gpt-5-mini")
+                .messages(List.of(
+                        new ChatMessage("system", systemMessage),
+                        new ChatMessage("user", question)
+                ))
+                .maxTokens(500)
+                .temperature(0.8)
+                .build();
+
+        var response = openAiService.createChatCompletion(request);
+        String feedback = response.getChoices().get(0).getMessage().getContent();
+
+        OlaHistoryJpaEntity saved = OlaHistoryJpaEntity.builder()
+                .portfolio(portfolio)
+                .question(question)
+                .answer(feedback)
+                .build();
+        olaRepository.save(saved);
+
+        return new OlaResponse(
+                saved.getId(),
+                saved.getPortfolio().getId(),
+                saved.getQuestion(),
+                feedback
+        );
+    }
+
+    public OlaListResponse historyOla(Long portfolioId) {
+        PortfolioJpaEntity portfolio = portfolioRepository.findById(portfolioId)
+                .orElseThrow(() -> new ExpectedException(HttpStatus.NOT_FOUND,"존재하지 않는 포트폴리오입니다"));
+        List<OlaHistoryJpaEntity> history = olaRepository.findByPortfolioOrderByIdDesc(portfolio);
+
+        List<OlaResponse> responses = history.stream()
+                .map(c -> new OlaResponse(
+                        c.getId(),
+                        c.getPortfolio().getId(),
+                        c.getQuestion(),
+                        c.getAnswer()
+                ))
+                .collect(Collectors.toList());
+
+        return new OlaListResponse(portfolioId, responses);
+    }
+}
