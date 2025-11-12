fa1e878a318bd7c3923bcd079aebb76b18ad59de
hongjm0912~ <s25036@gsm.hs.kr>
2025-11-12 23:43:43 +0900

    refactor(mainpage): 필터링 요청의 타입 검증 추가

--- src/main/java/org/example/story/domain/main/service/PortfolioListService.java
+++ src/main/java/org/example/story/domain/main/service/PortfolioListService.java
@@ -5,6 +5,8 @@ import org.example.story.domain.main.record.ListResponse;
 import org.example.story.domain.main.record.PortfolioListResponse;
 import org.example.story.domain.portfolio.entity.PortfolioJpaEntity;
 import org.example.story.domain.portfolio.repository.PortfolioRepository;
+import org.example.story.global.error.exception.ExpectedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,10 @@ public class PortfolioListService {
     private final PortfolioRepository portfolioRepository;
 
     public PortfolioListResponse portfolioFilter(Long lastId, int size, String type){
+        List<String> allowedSortFields = List.of("view", "like", "comment");
+        if (type != null && !allowedSortFields.contains(type)) {
+            throw new ExpectedException(HttpStatus.UNPROCESSABLE_ENTITY,"허용되지 않은 정렬 기준입니다: " + type);
+        }
         List<PortfolioJpaEntity> portfolios =
                 portfolioRepository.findWithCursor(lastId, size, type, true, null, true);
         List<ListResponse> listResponses = setList(portfolios);
