a837300e4f40967fba42760f219a2cc8f6a02b59
ZaMan-O <roblery128@gmail.com>
2025-11-24 23:49:16 +0900

    feat(exception): 값 검증 예외처리 추가

--- src/main/java/org/example/story/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/story/global/error/handler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package org.example.story.global.error.handler;
 
+import org.apache.coyote.Response;
 import org.example.story.global.error.data.response.ErrorResponse;
 import org.example.story.global.error.exception.ExpectedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,5 +18,10 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse(
                         e.getHttpStatus().value(), e.getMessage()
                 ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+        throw new ExpectedException(HttpStatus.BAD_REQUEST, "Null이 올 수 없는 속성에 Null이 들어왔습니다.");
     }
 }
