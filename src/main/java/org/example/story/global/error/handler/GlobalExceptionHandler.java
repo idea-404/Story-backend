1ec31e47440faa28edf02d0ef8f55e7e85439126
ZaMan-O <roblery128@gmail.com>
2025-11-25 19:47:12 +0900

    refactor(exception): 이메일 검증 예외 추가

--- src/main/java/org/example/story/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/story/global/error/handler/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ExpectedException.class)
@@ -22,7 +24,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
-        String message = "입력값이 유효하지 않습니다.";
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message));
