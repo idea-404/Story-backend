fe0b463a523cf515845f9064d52679f8103091cb
ZaMan-O <roblery128@gmail.com>
2025-11-25 19:59:50 +0900

    refactor(mail): 비동기 설정 추가

--- src/main/java/org/example/story/domain/user/service/SendEmailService.java
+++ src/main/java/org/example/story/domain/user/service/SendEmailService.java
@@ -3,6 +3,7 @@ package org.example.story.domain.user.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.story.global.error.exception.ExpectedException;
 import org.example.story.global.security.jwt.JwtTokenProvider;
 import org.example.story.global.security.jwt.record.common.TokenClaimsDto;
@@ -10,8 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SendEmailService {
@@ -21,6 +24,7 @@ public class SendEmailService {
     @Value("${base.url}")
     private String url;
 
+    @Async
     public void execute(
             String email, String subject, String body
     ) {
@@ -45,8 +49,7 @@ public class SendEmailService {
             helper.setText(body.formatted(verifyUrl), true);
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
-            throw new ExpectedException(
-                    HttpStatus.BAD_REQUEST, "메일 전송에 실패하였습니다 : " + e.getMessage());
+            log.error("이메일 전송 실패: {}", email, e);
         }
     }
 }
