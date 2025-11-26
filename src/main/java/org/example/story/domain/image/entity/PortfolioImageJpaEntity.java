1cf337d885c1a547d5a4cb24c139aeca19da74ee
hongjm0912~ <s25036@gsm.hs.kr>
2025-11-26 13:44:24 +0900

    feat(image): 이미지 관리 엔티티 및 콘픽 추가

---
+++ src/main/java/org/example/story/domain/image/entity/PortfolioImageJpaEntity.java
@@ -0,0 +1,29 @@
+package org.example.story.domain.image.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.story.domain.portfolio.entity.PortfolioJpaEntity;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Entity
+@Table(name = "portfolio_image")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PortfolioImageJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private PortfolioJpaEntity portfolio;
+
+    @Column(name = "imageUrl", nullable = false, length = 500)
+    private String imageUrl;
+
+}
