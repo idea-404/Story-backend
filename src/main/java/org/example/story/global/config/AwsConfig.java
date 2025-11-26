c18bb17c0736d6221559f67715f88728716fd9b3
hongjm0912~ <s25036@gsm.hs.kr>
2025-11-26 17:24:25 +0900

    refactor(image): AWS 리전 구조 변경

--- src/main/java/org/example/story/global/config/AwsConfig.java
+++ src/main/java/org/example/story/global/config/AwsConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AwsConfig {
 
     @Bean
-    public S3Presigner s3Presigner() {
+    public S3Presigner s3Presigner(
+            @Value("${spring.cloud.aws.region.static}")
+            String region) {
         return S3Presigner.builder()
-                .region(Region.AP_NORTHEAST_2)
+                .region(Region.of(region))
                 .build();
     }
 }
